257f6591faacf2bacde992b366c2f8550f5f927c
Dileep Kumar.S.R <dileepkumar1422002@gmail.com>
2022-07-03 12:39:58 +0530

    Create 1.RotateImage.java

---
+++ day2/1.RotateImage.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        
+        for(int i=0; i<matrix.length; i++) {
+            for(int j=i; j<matrix[0].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        
+        for(int i=0; i<matrix.length; i++) {
+            for(int j=0; j<matrix[0].length/2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length-j-1];
+                matrix[i][matrix.length-j-1] = temp;
+            }
+        }
+        
+    }
+}
+
+// Example 1:
+
+
+// Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+// Output: [[7,4,1],[8,5,2],[9,6,3]]
