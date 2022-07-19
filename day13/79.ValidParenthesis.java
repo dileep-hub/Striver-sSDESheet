7bd00edff096ac309eaccbb92e2debcab5002d0c
Dileep Kumar.S.R <dileepkumar1422002@gmail.com>
2022-07-19 11:55:27 +0530

    Create 79.ValidParenthesis.java

---
+++ day13/79.ValidParenthesis.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+        Stack<Character> stack = new Stack();
+        if(s.length() <= 1) return false;
+        
+        for(int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if(c == '{' || c == '[' || c == '(') {
+                stack.push(c);
+            }
+            else if(stack.empty()) return false;
+            else if( c == ')' && stack.pop() != '(') return false;
+            else if( c == '}' && stack.pop() != '{') return false;
+            else if( c == ']' && stack.pop() != '[') return false;
+        } 
+        return stack.isEmpty();
+    }
+}
